9273419addfe722226b1f101ad53b459cfe46e07
Wasim Odeh <wasim.odeh>
2018-02-24 14:18:05 +0200

    Client development

--- WebContent/app/controllers.js
+++ WebContent/app/controllers.js
@@ -8,3 +8,139 @@ booksForAll.controller('mainController', ['$scope', '$rootScope', '$location', '
 		$location.path(newValue);
 	});
 }])
+
+// Login controller
+.controller('loginController', ['$scope', '$rootScope', '$timeout', '$http', 'notifyService', function($scope, $rootScope, $timeout, $http, notifyService) {
+	
+	$("body").css({
+		"background": "url(assets/images/login.png)",
+		"background-size": "cover"
+	});
+
+	$scope.login = function () {
+		var user = {
+			"username": $scope.username,
+			"password": $scope.password
+		};
+
+		$http({
+			method: 'POST',
+			url: 'login',
+			headers: {'Content-Type' : "application/json; charset=utf-8"},
+			data: user
+		}).then(
+			function (success) {
+				var data = success.data;
+				notifyService.alert({
+					"status": data.status,
+					"selector": data.notification.selector,
+					"message": data.notification.message
+				});
+				if (data.status == "success") {
+					$timeout(function () {
+						$rootScope.route = data.route;
+						$rootScope.user = data.user;
+					}, 2500);
+				}
+			},
+			function (failure) {
+				console.log(failure.data);
+			}
+		);
+	};
+}])
+
+// Register controller
+.controller('registerController', ['$scope', '$rootScope', '$timeout', '$http', '$filter', 'notifyService', function($scope, $rootScope, $timeout, $http, $filter, notifyService) {
+	// Scope variables
+	$scope.disabled = true;
+
+	// Scope methods
+	$scope.checkButton = function () {
+		$scope.disabled = 
+			$scope.validUsername != "glyphicon glyphicon-ok-circle" || $scope.validNickname != "glyphicon glyphicon-ok-circle"
+			||
+			$scope.username == "" || $scope.nickname == "" ||	$scope.password == ""
+			||
+			$scope.username == undefined || $scope.nickname == undefined ||	$scope.password == undefined;
+	};
+
+	$scope.register = function () {
+		var user = {
+			"username": $scope.username,
+			"password": $scope.password,
+			"nickName": $scope.nickname,
+			"description": $scope.description,
+			"photoUrl": $scope.photoUrl !== undefined ? $scope.photoUrl : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSMyKQ_ZaKgbgQ6PE--NyftpawhbFDuv0lIZAslbH_o5QVS3KY9wHo87AqxyQ",
+			"status" : true
+		};
+
+		$http({
+			method: 'POST',
+			url: 'register',
+			headers: {'Content-Type' : "application/json; charset=utf-8"},
+			data: user
+		}).then(
+			function (success) {
+				var data = success.data;
+				notifyService.alert({
+					"status": data.status,
+					"selector": data.notification.selector,
+					"message": data.notification.message
+				});
+				if (data.status == "success") {
+					$timeout(function () {
+						$(".modal-backdrop").css({display: 'none'});
+						$rootScope.route = data.route;
+						$rootScope.user = data.user;	
+					}, 2500);
+				}
+			},
+			function (failure) {
+				console.log(failure.data);
+			}
+		);
+	};
+
+	$scope.validateUsername = function () {
+		if ($scope.username != "" && $scope.username !== undefined) {
+			$http({
+				method: 'POST',
+				url: 'validate',
+				headers: {'Content-Type' : "application/json; charset=utf-8"},
+				data: {"username": $scope.username}
+			}).then(
+				function (success) {
+					$scope.validUsername = "glyphicon glyphicon-" + success.data.valid + "-circle";
+					$scope.checkButton();
+				},
+				function (failure) {
+					console.log(failure.data);
+				}
+			);
+		} else {
+			$scope.validUsername = "";
+		}
+	};
+
+	$scope.validateNickname = function () {
+		if ($scope.nickname != "" && $scope.nickname !== undefined) {
+			$http({
+				method: 'POST',
+				url: 'validate',
+				headers: {'Content-Type' : "application/json; charset=utf-8"},
+				data: {"nickName": $scope.nickname}
+			}).then(
+				function (success) {
+					$scope.validNickname = "glyphicon glyphicon-" + success.data.valid + "-circle";
+					$scope.checkButton();
+				},
+				function (failure) {
+					console.log(failure.data);
+				}
+			);
+		} else {
+			$scope.validNickname = "";
+		}
+	};
+}])
