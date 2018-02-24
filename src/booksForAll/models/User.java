4deebf3e1c3c287f55fc12b01cc5bdbabc8c8c64
Wasim odeh <odehwassim@gmail.com>
2018-02-24 12:14:58 +0200

    fixed login servlet & and setters to User model

--- src/booksForAll/models/User.java
+++ src/booksForAll/models/User.java
@@ -11,6 +11,8 @@ public class User {
 	private String photoUrl;
 	private boolean status;
 	
+	public User() {}
+	
 	public User(String userName,String password,boolean isAdmin,String NickName,String email,String telephone,String description,String photoUrl,boolean status) {
 		this.userName=userName;
 		this.password = password;
@@ -27,33 +29,64 @@ public class User {
 		return userName;
 	}
 	
+	public void setUserName(String userName) {
+		this.userName=userName;
+	}
+	
 	public String getPassword() {
 		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password=password;
 	}
 	
 	public String getNickName() {
 		return NickName;
 	}
 	
+	public void setNickName(String nickName) {
+		this.NickName=nickName;
+	}
+	
 	public boolean getIsAdmin() {
 		return isAdmin;
+	}
+	
+	public void setIsAdmin(boolean isAdmin) {
+		this.isAdmin=isAdmin;
 	}
 	
 	public String getEmail() {
 		return email;
 	}
 	
+	public void setEmail(String email) {
+		this.email=email;
+	}
 	
 	public String getTelephone() {
 		return telephone;
+	}
+	
+	public void setTelephone(String telephone) {
+		this.telephone=telephone;
 	}
 	
 	public String getDescription() {
 		return description;
 	}
 	
+	public void setDescription(String description) {
+		this.description=description;
+	}
+	
 	public String getPhotoUrl() {
 		return photoUrl;
+	}
+	
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl=photoUrl;
 	}
 	
 	public boolean getStatus() {
