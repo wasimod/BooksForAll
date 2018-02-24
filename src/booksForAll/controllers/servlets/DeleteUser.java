150f0cca25a47074bdde519122dbef47ed226fe5
Wasim odeh <odehwassim@gmail.com>
2018-02-24 15:58:57 +0200

    add delete user servlet & fix mistakes in some statements

---
+++ src/booksForAll/controllers/servlets/DeleteUser.java
@@ -0,0 +1,120 @@
+package booksForAll.controllers.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import booksForAll.globals.Globals;
+import booksForAll.models.User;
+
+/**
+ * Servlet implementation class DeleteUser
+ */
+@WebServlet("/DeleteUser")
+public class DeleteUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteUser() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		handleRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		handleRequest(request, response);
+	}
+
+	/**
+	 * 	
+	 * Handles an HTTP request.
+	 * Delete user.
+	 * <p>
+	 * <b>Used methods:</b>
+	 * <br/>
+	 * <dd>{@link #delete(User)} - Delete the user from the database.</dd>
+	 * @param request Http request
+	 * @param response Http response
+	 * @throws ServletException
+	 * @throws IOException
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 *
+	 */
+	private void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Gson gson = new GsonBuilder().create();
+		// Convert JSON object from request input to User object
+		User user = gson.fromJson(request.getReader(), User.class);
+		// Prepare a JSON to be forwarded to a new servlet or returned in the response
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json; charset=UTF-8");
+		String data;
+		if (delete(user)) {
+			data = "{"
+				+ 		"\"status\": \"success\","
+				+ 		"\"route\": \"messages\","
+				+ 		"\"notification\": {"
+				+ 			"\"selector\": \".user-delete-notification\","
+				+ 			"\"message\": \"You've successfully deleted the user.\""
+				+ 		"}"
+				+  "}"
+				;
+		} else {
+			data = "{"
+				+ 		"\"status\": \"danger\","
+				+ 		"\"route\": \"messages\","
+				+ 		"\"notification\": {"
+				+ 			"\"selector\": \".user-delete-notification\","
+				+ 			"\"message\": \"Something went wrong, please try again later\""
+				+ 		"}"
+				+  "}"
+				;
+		}
+		out.println(data);
+		out.close();
+	}
+	/**
+	 * A method to delete a user from the database.
+	 * @param user {@link booksForAll.models.User} object that contain the user data.
+	 * @return True in case the user deleted successfully, False otherwise.
+	 */
+	private boolean delete (User user) {
+		int rows = 0;
+		try {
+			Connection connection = Globals.database.getConnection();
+			PreparedStatement statement = connection.prepareStatement(Globals.DELETE_USER);
+			
+			statement.setString(1, user.getUserName());
+			
+			rows = statement.executeUpdate();
+			connection.commit();
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+			System.out.println("An error has occured while trying to execute the query!");
+		}
+		
+		return rows > 0;
+	}
+}
