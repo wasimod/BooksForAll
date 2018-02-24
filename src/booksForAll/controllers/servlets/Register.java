3da55756de69d305ec9734eb5771b5da240dbf6a
Wasim Odeh <wasim.odeh>
2018-02-24 15:03:28 +0200

    changed register url path

--- src/booksForAll/controllers/servlets/Register.java
+++ src/booksForAll/controllers/servlets/Register.java
@@ -22,7 +22,7 @@ import booksForAll.models.User;
 /**
  * Servlet implementation class Register
  */
-@WebServlet("/Register")
+@WebServlet("/register")
 public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
